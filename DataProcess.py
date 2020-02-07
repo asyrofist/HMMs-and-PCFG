390a86987517c8b98cf570cce506090191297ff9
Isabelle Lee <isabelll@amazon.com>
2020-02-07 01:55:19 -0800

    modified sentence tokenization, added corpus tokenization

--- DataProcess.py
+++ DataProcess.py
@@ -5,10 +5,29 @@ def read_file(path):
     return f.read().splitlines()
 
 def tokenize_sentence(sentence):
+    """
+    input : sentence in sting format
+    output : list of tuples, (word, pos)
+    """
     #remove the outer [[ ]] 
     sentence = sentence[2:-2]
     tokens = sentence.split("], [")
-    return tokens
+    processed_tokens = []
+    for token in tokens:
+        [word, pos]= token.split(", ")
+        # remove quotations around each tokens
+        processed_tokens.append((word[1:-1] ,pos[1:-1]))
+    return processed_tokens
+
+def tokenize_corpus(corpus):
+    """
+    input : corpus as list of sentences
+    output : list of lists of tuples (word, pos)
+    """
+    processed_corpus = []
+    for sentence in corpus:
+        processed_corpus.append(tokenize_sentence(sentence))
+    return processed_corpus
 
 def remove_punctuation(sentence):
     return sentence.translate(str.maketrans('','',string.punctuation))

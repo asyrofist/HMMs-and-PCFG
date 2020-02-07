5daf8b7de3ca3d4a77220a55f9013831a271a7a4
Isabelle Lee <isabelll@amazon.com>
2020-02-07 03:01:31 -0800

    added bigram count function

--- HMMs/bigram_hmms.py
+++ HMMs/bigram_hmms.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 
 def generate_bigrams(sentence):
     bigrams = []
@@ -5,3 +7,11 @@ def generate_bigrams(sentence):
         bigram = (sentence[i-1],sentence[i])
         bigrams.append(bigram)
     return bigrams
+
+def count_bigrams(corpus):
+    bigram_count = defaultdict(int)
+    for sentence in corpus:
+        bigrams = generate_bigrams(sentences)
+        for bigram in bigrams:
+            bigram_count[bigram] += 1
+    return bigram_count

d0c4b8b57b039bf6af82c34ef411dde441b3bb3d
Isabelle Lee <isabelll@amazon.com>
2020-02-08 12:26:14 -0800

    added trigram probability

--- HMMs/trigram_hmms.py
+++ HMMs/trigram_hmms.py
@@ -1,4 +1,5 @@
-
+from bigrams_hmms import generate_bigrams
+from collections import defaultdict
 
 def generate_trigrams(sentence):
     trigrams = []
@@ -6,3 +7,7 @@ def generate_trigrams(sentence):
         trigram = (sentence[i-2], sentence[i-1], sentence[i])
         trigrams.append(trigram)
     return trigrams
+
+def trigram_proba(u,v,w, bigram_count, trigram_count, k = 0, V):
+    p = (trigram_count(u,v,w)+k)/(bigram_count(u,v)+V)
+    return p

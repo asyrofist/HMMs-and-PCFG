a30df76d1d6b0302137527790a2e3953dc9ad112
Isabelle Lee <isabelll@amazon.com>
2020-02-09 16:11:45 -0800

    added class for bigramhmms

--- HMMs/bigram_hmms.py
+++ HMMs/bigram_hmms.py
@@ -1,6 +1,8 @@
 import sys, os
 sys.path.append("~/Desktop/nlp/HMMs-and-PCFG/HMMs")
+sys.path.append("~/Desktop/nlp/HMMs-and-PCFG")
 
+from DataProcess import read_file, count_unigrams, extract_vocab, tokenize_corpus, set_unk
 from collections import defaultdict
 
 def generate_bigrams(sentence):
@@ -27,4 +29,27 @@ def bigram_proba(u, v, unigram_count, bigram_count, V, k = 0):
     else:
         p = (bigram_count[(u,v)]+k)/(unigram_count[u]+k*V)
     return p
+
+def process_train_corpus(train_path):
+    train_file = read_file(train_path)
+    train_raw = tokenize_corpus(train_file,2)
+    unigrams = count_unigrams(train_raw)
+    vocab, V = extract_vocab(unigrams)
+    train_processed = set_unk(train_raw, vocab)
+    return vocab, V, train_processed
+
+class bigramHMMs:
+    def __init__(self, train_path, dev_path, test_path):
+        # extract vocabulary from train files, process training file with UNK
+        self.vocab, self.V, self.train_file = process_train_corpus(train_path)
+        # process raw dev and test files
+        self.dev_file = read_file(dev_path)
+        self.test_file = read_file(test_path)
+        
+        # count unigrams and bigrams
+        self.unigram_count = count_unigrams(self.train_file)
+        self.bigram_count = count_bigrams(self.train_file)
+
+
+
 
